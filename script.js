55708239b5fb03e6a0e34c99d0993d8a80f2eb09
Pete Brennan <pweda64@hotmail.com>
2020-04-05 11:52:36 +0800

    styling amendments to page

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  $("#portfolio").on("click", function() {
+  $(".portfolio-btn").on("click", function() {
     $(window).scrollTop(0);
     $(".home-page").addClass("d-none");
     $(".contact").addClass("d-none");
     $(".contact").removeClass("d-block");
     $(".portfolio").addClass("d-block");
   });
-  $("#contact").on("click", function() {
+  $(".contact-btn").on("click", function() {
     $(window).scrollTop(0);
     $(".home-page").addClass("d-none");
     $(".portfolio").addClass("d-none");
